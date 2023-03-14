3f0894891328d927af42a98b811842ef058b4fec
MXMxRazer <var.kritan@gmail.com>
2023-03-14 15:46:58 -0400

    updated form of timer

--- src/timer/main.js
+++ src/timer/main.js
@@ -1,165 +1,168 @@
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 import ringer from './beep.mp3';
 import '../index.css'
 
 export default function Imp() {
-    const audio = new Audio(ringer); 
 
-    const [values, funcs] = useState(
-        {
-            hours: '00',
-            mins: '00', 
-            secs: '00', 
-        }
-    )
+    const [start, setStart] = useState(false); 
+    const [showTimer, setShowTimer] = useState(false); 
 
-    const [val, fun] = useState({
-        h: 0,  
-        m: 0,
-        s: 0,
-    })
+    const [timerState, setTimerSet] = useState({
+        year: "", 
+        month: "",
+        day: "", 
+        hour: "", 
+        min: "", 
+        sec: ""
+    }); 
 
-    const [tune, tFunc] = useState(false);
-    const [rSet, rFunc] = useState(false);
-    const [click, cFunc] = useState(true);
-    
-    const clicking = () => {
-        cFunc (
-            prev => !prev
-        )
+    // const deadline = 'March, 15, 2023 01:23:00'; 
+    const deadline = `${timerState.month}, ${timerState.day}, ${timerState.year} ${timerState.hour}:${timerState.min}:${timerState.sec}`;
+
+    const getReset = () => {
+
+        setStart(prev => !prev);
+
+        setTimerSet(prev => ({
+            year: "0000", 
+            month: "00", 
+            day: "00", 
+            hour: "00", 
+            min: "00", 
+            sec: "00"
+        }))
     }
 
-    const typing = (event) => {
-        val.h = event.target.value
+    const getTime = () => {
+        const time = Date.parse(deadline) - Date.now(); 
+
+        setTimerSet(prev => ({
+            day: Math.floor(time / (1000 * 60 * 60 * 24)), 
+            hour: Math.floor((time / (1000 * 60 * 60)) % 24), 
+            min: Math.floor((time / 1000 / 60) % 60), 
+            sec: Math.floor((time / 1000) % 60)
+        }))
+
+        setShowTimer(prev => true); 
+
     }
-    
-    const typing2 = (event) => {
-        val.m = event.target.value
-    }
-    const typing3 = (event) => {
-        val.s = event.target.value
-    } 
 
+    useEffect(() => {
 
-        const turning = () => {
-            window.localStorage.setItem("Time", JSON.stringify(values));
-            console.log("here is the Function");  
-                tFunc (
-                    prev => !prev
-                ); 
-        };
+        console.log(timerState); 
 
-        const reset = () => {
-            rFunc (
-                prev => !prev
-            );
-            funcs (
-                prev => {
-                    return {
-                        secs: '00', 
-                    }
-                }
-            )
-            console.log(values); 
-        }
+            if (start) {
+                const interval = setInterval(() => getTime(deadline), 1000); 
+            
+                return () => clearInterval(interval); 
+            }
 
-        const [tttime, Tfunc] = useState(false); 
-        var counter = 0, temp = val.s; 
-        var a = (values.secs == '00') ? 0 : values.secs; 
-        var b = (values.mins == '00') ? 0 : values.mins; 
-        var c = (values.hours == '00') ? 0 : values.hours;  
-        var seconds  = 0;  
-        useEffect( ()=> {
-            const interval = setInterval(() => { 
-                if (click) {
-                    clearInterval(interval)
-                }else {
-                    if (val.h == c && val.m == b && val.s == a) {
-                        audio.play(); 
-                        console.log("Time's UP!");
-                        a = 0;
-                        counter++;  
-                        if (counter % 2 === 0) {
-                            console.log("break")
-                            val.s = 5; 
-                        } else {
-                            val.s = temp;  
-                        }
-                        if (counter === 4) {
-                            clearInterval(interval);
-                            console.log("Cleared!");
-                            Tfunc (
-                                prev => true
-                            )
-                        }
-                    }    
-                if (tune) {
-                    clearInterval(interval);
-                }else {
-                    a++;   
-                }
-                if (a > 60) { 
-                    audio.play();
-                    b++; 
-                    values.mins = b;
-                    a = 0;  
-                    if (b > 60) {
-                        c++; 
-                        values.hours = c;
-                    }
-                }}
-                funcs (
-                    prev => {
-                        return {
-                            ...prev, 
-                            hours: (c < 10) ? '0'+c : c,  
-                            mins:  (b < 10) ? '0'+b : b,
-                            secs:  (a < 10) ? '0'+a : a,
-                        }
-                    }
-                )
-                ;
-            }, 1000); 
-            return () => clearInterval(interval); 
-        }, [tune, click]); 
-    
+    }, [start]); 
 
     return (
         <div className="main">
         <form className="form">
-            <h3 id="h">Hours: </h3>
+            <h3>Year: </h3>
+            <input
+            id="inputs"        
+            name="year"
+            onChange={e => {
+                setTimerSet(prev => ({
+                    ...prev, 
+                    year: (e.target.value)
+                }))
+            }}
+            ></input> 
+            <h3>Month: </h3>
+            <input
+            id="inputs"        
+            name="month"
+            onChange={e => {
+                setTimerSet(prev => ({
+                    ...prev, 
+                    month: (e.target.value)
+                }))
+            }}
+            ></input> 
+            <h3>Day: </h3>
+            <input
+            id="inputs"        
+            name="day"
+            onChange={e => {
+                setTimerSet(prev => ({
+                    ...prev, 
+                    day: (e.target.value)
+                }))
+            }}
+            ></input> 
+            <h3 id="h">Hour: </h3>
             <input
             id="inputs"        
             name="hour"
-            onChange={typing}
+            onChange={e => {
+                setTimerSet(prev => ({
+                    ...prev, 
+                    hour: parseInt(e.target.value)
+                }))
+            }}
             ></input> 
-            <h3 id="m">mins: </h3>
+            <h3 id="m">Minute: </h3>
             <input
             id="inputs"
-            onChange={typing2}
-            name="minute"
+            name="min"
+            onChange={e => {
+                setTimerSet(prev => ({
+                    ...prev, 
+                    min: e.target.value
+                }))
+            }}
             ></input> 
-            <h3 id="s">sec: </h3>
+            <h3 id="s">Second: </h3>
             <input
             id="inputs"
-            onChange={typing3}
-            name="second"
+            name="sec"
+            onChange={e => {
+                setTimerSet(prev => ({
+                    ...prev, 
+                    sec: e.target.value
+                }))
+            }}
             ></input>          
         </form>
             <button onClick={() => {
-                clicking(); 
+                setStart(prev => !prev); 
             }}
             className="click"
             >
                 Click me!
             </button>
             <div className="clock">
-            <h1 id="clock-values">{values.hours} : {values.mins} {values.secs} </h1>
+            {
+                showTimer &&
+                <div className="super-clock-box">
+                    <div class="clock-box">
+                        <h3>Days: </h3>
+                        <h1 id="clock-values">{timerState.day}</h1>
+                    </div>
+                    <div className="super-clock-box">
+                        <h3>Hour: Min: Sec</h3>
+                        <h1 id="clock-values">{String(timerState.hour).padStart(2, '0')} : {String(timerState.min).padStart(2, '0')} : {String(timerState.sec).padStart(2, '0')} </h1>
+                    </div>
+                    <div className="buttons">
+                        <button
+                            className="click"
+                            onClick={e => getReset()}
+                        >
+                            Reset
+                        </button>
+                    </div>
+                </div>
+            }
             </div>
-            {tttime && <div className="time-up">TIME'S UP !</div>}
+            {/* {tttime && <div className="time-up">TIME'S UP !</div>} */}
             <div className="last-buttons">
-            {tune ? <button onClick={turning} className="btm-button" id="b1">Start</button> : <button onClick={turning} className="btm-button" id="b1">Pause</button>}
-            <button onClick={reset} className="btm-button" id="b2">Reset</button>
+            {/* {tune ? <button onClick={turning} className="btm-button" id="b1">Start</button> : <button onClick={turning} className="btm-button" id="b1">Pause</button>} */}
+            {/* <button onClick={reset} className="btm-button" id="b2">Reset</button> */}
             </div>
         </div>
     )
